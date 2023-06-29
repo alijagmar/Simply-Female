349af8a122da58238d864bd72ba03403c25a8dab
Alia Marzyie <marzyiealia@gmail.com>
2023-06-29 09:53:58 -0400

    Initial Commit

---
+++ Simply Female/Simply_FemaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simply_FemaleApp.swift
+//  Simply Female
+//
+//  Created by Alia Marzyie on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Simply_FemaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
